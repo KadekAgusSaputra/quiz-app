44814b46efa15e69f1fa1ab94240ca1af8f35cf0
KadekAgusSaputra <aldhypahyana99@gmail.com>
2024-01-21 13:13:20 +0800

    baru ini

--- scripts/app.dashboard.js
+++ scripts/app.dashboard.js
@@ -1,8 +1,35 @@
 // == TEMPLATE QUIZ ITEM ==
 
-// generateQuizItem({
-//   id: question.id,
-//   question: question.jokes,
-//   answer: question.answer,
-//   category: question.category,
-// });
+
+import { getQuestions, createQuestion } from './api.js'
+import { generateQuizItem } from './utils/index.js'
+const quizContent = document.getElementById("quiz-content")
+
+document.addEventListener("DOMContentLoaded", () => {
+    async function handAllQuestion(){
+        try {
+            const question = await getQuestions();
+            // ! misalnya question gak ada
+            if(!question) return;
+
+            quizContent.innerHTML = question.map((question) => {
+                return generateQuizItem({
+                    id: question.id,
+                    question: question.jokes,
+                    answer: question.answer,
+                    category: question.category,
+                  });
+                
+            }).join("")
+            // misalnya question ada, maka tampilkan
+            console.log({question});
+
+        }catch (eror) {
+            console.error("ada eror nih : ", {
+                eror,
+            })
+        }
+    }
+
+    handAllQuestion();
+})
